0141dd082de24f935f6db310cb46e9a6726da91a
JulianaPG2 <164351097+JulianaPG2@users.noreply.github.com>
2026-03-10 18:20:08 -0500

    Create webpage.h

---
+++ webpage.h
@@ -0,0 +1,24 @@
+const char index_html[] PROGMEM = R"rawliteral(
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Spectrometer</title>
+</head>
+
+<body>
+
+<h2>Control del espectrometro</h2>
+
+<a href="/start">Iniciar mediciones</a>
+
+<br><br>
+
+<a href="/download">Descargar datos CSV</a>
+
+</body>
+
+</html>
+
+)rawliteral";
