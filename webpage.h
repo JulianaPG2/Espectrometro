253b60908d7f915c082bb7604e6b3c00c3a4214c
JulianaPG2 <164351097+JulianaPG2@users.noreply.github.com>
2026-03-10 18:41:09 -0500

    Update webpage.h

--- webpage.h
+++ webpage.h
@@ -9,7 +9,42 @@ const char index_html[] PROGMEM = R"rawliteral(
 
 <body>
 
-<h2>Control del espectrometro</h2>
+<h2>Configuracion WiFi</h2>
+
+<form action="/setwifi" method="POST">
+
+SSID:<br>
+<input type="text" name="ssid"><br>
+
+Password:<br>
+<input type="text" name="password"><br>
+
+<input type="submit" value="Guardar">
+
+</form>
+
+<hr>
+
+<h2>Configuracion Sensor</h2>
+
+<form action="/setsensor" method="POST">
+
+Numero de muestras:<br>
+<input type="number" name="samples"><br>
+
+Integracion:<br>
+<input type="number" name="integration"><br>
+
+Ganancia:<br>
+<input type="number" name="gain"><br>
+
+<input type="submit" value="Guardar">
+
+</form>
+
+<hr>
+
+<h2>Control</h2>
 
 <a href="/start">Iniciar mediciones</a>
 
