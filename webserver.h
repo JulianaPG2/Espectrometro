e26f086882b4c92eaf28db18a43dc0ca55c86136
JulianaPG2 <164351097+JulianaPG2@users.noreply.github.com>
2026-03-10 18:39:52 -0500

    Update webserver.h

--- webserver.h
+++ webserver.h
@@ -6,6 +6,27 @@ request->send(200,"text/html",index_html);
 
 });
 
+server.on("/setwifi", HTTP_POST, [](AsyncWebServerRequest *request){
+
+ssid = request->getParam("ssid",true)->value();
+password = request->getParam("password",true)->value();
+
+Serial.println("WiFi guardado");
+
+request->send(200,"text/plain","Guardado");
+
+});
+
+server.on("/setsensor", HTTP_POST, [](AsyncWebServerRequest *request){
+
+config.samples = request->getParam("samples",true)->value().toInt();
+config.integration = request->getParam("integration",true)->value().toInt();
+config.gain = request->getParam("gain",true)->value().toInt();
+
+request->send(200,"text/plain","Configuracion guardada");
+
+});
+
 server.on("/start", HTTP_GET, [](AsyncWebServerRequest *request){
 
 takeMeasurements();
